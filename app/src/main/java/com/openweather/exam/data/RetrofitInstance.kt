a3d7d5a398fcd2ef297aa46eb6f2ec8afba07600
kenju69 <kenju.satanic@gmail.com>
2025-09-01 17:55:56 +0800

    Update RetrofitInstance.kt

--- app/src/main/java/com/openweather/exam/data/RetrofitInstance.kt
+++ app/src/main/java/com/openweather/exam/data/RetrofitInstance.kt
@@ -1,13 +1,17 @@
 package com.openweather.exam.data
 
 import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 object RetrofitInstance {
+
     private const val BASE_URL = "https://api.openweathermap.org/"
 
-    private val moshi = Moshi.Builder().build()
+    private val moshi: Moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
 
     val api: WeatherApiService by lazy {
         Retrofit.Builder()
